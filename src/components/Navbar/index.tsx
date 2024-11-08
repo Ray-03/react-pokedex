8f06f32cf59898f3b3a17cc692e0dfef442dc6f1
ray-03 <rinaldy.richard.03@gmail.com>
2024-11-08 22:15:49 +0700

    feat: add navbar

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import {
+  Box,
+  Flex,
+  HStack,
+  IconButton,
+  useDisclosure,
+  Stack,
+  Image,
+  Link,
+  VStack,
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import { K_PADDING } from "../../constant";
+import NavURLBar from "./NavURLBar";
+
+export default function Navbar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <Box as="header" px={K_PADDING}>
+      <Flex minH={16} alignItems={"center"} justifyContent={"space-between"}>
+        <IconButton
+          size={"md"}
+          aria-label={"Open Menu"}
+          display={{ md: "none" }}
+          onClick={isOpen ? onClose : onOpen}
+        >
+          {isOpen ? <CloseIcon /> : <HamburgerIcon />}
+        </IconButton>
+        <HStack spacing={8} alignItems={"center"}>
+          <Link href={"/"}>
+            <HStack>
+              <Image
+                src="/pokedex.png"
+                alt={"pokedex"}
+                objectFit="contain"
+                maxH={12}
+              />
+              <Image
+                src="/pokemon.png"
+                alt={"pokemon"}
+                objectFit="contain"
+                maxH={12}
+              />
+            </HStack>
+          </Link>
+
+          <HStack as={"nav"} spacing={4} display={{ base: "none", md: "flex" }}>
+            <NavURLBar />
+          </HStack>
+        </HStack>
+        <Flex alignItems={"center"}>{/*  */}</Flex>
+      </Flex>
+
+      {/* Mobile view */}
+      {isOpen ? (
+        <Box pb={4} display={{ md: "none" }}>
+          <Stack as={"nav"} spacing={4}>
+            <NavURLBar />
+          </Stack>
+        </Box>
+      ) : null}
+    </Box>
+  );
+}
