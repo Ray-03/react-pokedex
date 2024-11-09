070e56901d4a92dec51e87af8e7d3df4f14756e8
ray-03 <rinaldy.richard.03@gmail.com>
2024-11-09 11:53:16 +0700

    feat: add redux

---
+++ src/redux/features/apiSlice.ts
@@ -0,0 +1,25 @@
+import { IPokemon } from "@/interface/pokemon";
+import { IPokeapiResponse, IPokemonListItem } from "@/interface/pokemonList";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://pokeapi.co/api/v2/" }), // Adjust base URL as needed
+  endpoints: (builder) => ({
+    getPokemons: builder.query<
+      IPokemonListItem[],
+      { limit?: number; page?: number }
+    >({
+      query: ({ limit = 20, page = 1 }) =>
+        `pokemon?limit=${limit}&offset=${(page - 1) * limit}`,
+      transformResponse(baseQueryReturnValue: IPokeapiResponse, meta, arg) {
+        return baseQueryReturnValue.results;
+      },
+    }),
+    getPokemon: builder.query<IPokemon, { name: string }>({
+      query: ({ name }) => `pokemon/${name}`,
+    }),
+  }),
+});
+
+export const { useGetPokemonsQuery, useGetPokemonQuery } = apiSlice;
